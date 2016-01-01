eb5dece82ef0e57f9ab50a870251b6f92b7ab48b
kd <krakensden@krakensden.com>
2016-01-01 14:41:43 -0800

    Add venue introspection stuff

--- src/cmd/sfbase/main.go
+++ src/cmd/sfbase/main.go
@@ -7,9 +7,7 @@ import (
 )
 
 func main() {
-	sfc := &sflib.StockfighterClient{
-		Api_key: os.Getenv("STARFIGHTER_KEY"),
-	}
+	sfc := sflib.NewStockfighterClient(os.Getenv("STARFIGHTER_KEY"))
 	hb, err := sfc.GetHeartbeat()
 	if err != nil {
 		fmt.Println(err)
@@ -20,5 +18,25 @@ func main() {
 		fmt.Println(hb.Error)
 		os.Exit(1)
 		return
+	}
+	cv, err := sfc.CheckVenue(os.Getenv("STOCKFIGHTER_VENUE"))
+	if err != nil || !cv.Ok {
+		if err != nil {
+			fmt.Println("Connectivity broken", err)
+		}
+		fmt.Println("Venue isn't up")
+	}
+	fmt.Println("Venue is ", cv.Venue)
+	// Get stocks
+	vs, err := sfc.GetVenueStocks(os.Getenv("STOCKFIGHTER_VENUE"))
+	if err != nil || !vs.Ok {
+		if err != nil {
+			fmt.Println("Connectivity broken", err)
+		}
+		fmt.Println("Venue isn't up")
+	}
+	fmt.Println("Available stocks")
+	for _, i := range vs.Symbols {
+		fmt.Println("Venue is ", i.Name, " :: ", i.Symbol)
 	}
 }
--- src/sflib/sflib.go
+++ src/sflib/sflib.go
@@ -2,7 +2,7 @@ package sflib
 
 import (
 	"encoding/json"
-	//"fmt"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -12,17 +12,42 @@ type Heartbeat struct {
 	Error string `json:"error"`
 }
 
+type CheckVenue struct {
+	Ok    bool   `json:"ok"`
+	Venue string `json:"venue"`
+}
+
+type StockSymbol struct {
+	Name, Symbol string
+}
+
+type VenueStocks struct {
+	Ok      bool
+	Symbols []StockSymbol
+}
+
 type StockfighterClient struct {
-	httpclient http.Client
+	httpclient *http.Client
 	Api_key    string
+}
+
+func NewStockfighterClient(api_key string) *StockfighterClient {
+	sfc := &StockfighterClient{
+		Api_key: api_key,
+	}
+	sfc.httpclient = &http.Client{}
+	return sfc
+}
+
+func newHttpReq(method, url string, body *interface{}) (*interface{}, error) {
+	return nil, nil
 }
 
 func (this *StockfighterClient) GetHeartbeat() (Heartbeat, error) {
 	var hb Heartbeat
-	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://api.stockfighter.io/ob/api/heartbeat", nil)
 	req.Header.Add("X-Starfighter-Authorization", this.Api_key)
-	resp, err := client.Do(req)
+	resp, err := this.httpclient.Do(req)
 	if err != nil {
 		return hb, err
 	}
@@ -36,4 +61,48 @@ func (this *StockfighterClient) GetHeartbeat() (Heartbeat, error) {
 		return hb, err
 	}
 	return hb, err
+}
+
+func (this *StockfighterClient) CheckVenue(venue string) (CheckVenue, error) {
+	var cv CheckVenue
+	req, err := http.NewRequest("GET",
+		fmt.Sprintf("https://api.stockfighter.io/ob/api/venues/%s/heartbeat", venue),
+		nil)
+	req.Header.Add("X-Starfighter-Authorization", this.Api_key)
+	resp, err := this.httpclient.Do(req)
+	if err != nil {
+		return cv, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return cv, err
+	}
+	err = json.Unmarshal(body, &cv)
+	if err != nil {
+		return cv, err
+	}
+	return cv, err
+}
+
+func (this *StockfighterClient) GetVenueStocks(venue string) (VenueStocks, error) {
+	var vs VenueStocks
+	req, err := http.NewRequest("GET",
+		fmt.Sprintf("https://api.stockfighter.io/ob/api/venues/%s/stocks", venue),
+		nil)
+	req.Header.Add("X-Starfighter-Authorization", this.Api_key)
+	resp, err := this.httpclient.Do(req)
+	if err != nil {
+		return vs, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return vs, err
+	}
+	err = json.Unmarshal(body, &vs)
+	if err != nil {
+		return vs, err
+	}
+	return vs, err
 }
