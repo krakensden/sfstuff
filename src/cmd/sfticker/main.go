4821911fe6d52f9c914dd06e7c22d71b602b2429
kd <krakensden@krakensden.com>
2016-02-14 11:50:26 -0800

    Factor out boilerplate

--- src/cmd/sfticker/main.go
+++ src/cmd/sfticker/main.go
@@ -8,34 +8,10 @@ import (
 )
 
 func main() {
-	sfc := sflib.NewStockfighterClient(os.Getenv("STARFIGHTER_KEY"))
-	hb, err := sfc.GetHeartbeat()
+	sfc, vs, err := sflib.Setup()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-	fmt.Fprintln(os.Stderr, "Heartbeat API is OK", hb.Ok)
-	if !hb.Ok {
-		fmt.Fprintln(os.Stderr, hb.Error)
+		fmt.Fprintln(os.Stderr, "setup failed", err)
 		os.Exit(1)
-		return
-	}
-	fmt.Fprintln(os.Stderr, "Raw venue is", os.Getenv("STOCKFIGHTER_VENUE"))
-	cv, err := sfc.CheckVenue(os.Getenv("STOCKFIGHTER_VENUE"))
-	if err != nil || !cv.Ok {
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Connectivity broken", err)
-		}
-		fmt.Fprintln(os.Stderr, "Venue isn't up")
-	}
-	fmt.Fprintln(os.Stderr, "Venue is ", cv.Venue)
-	// Get stocks
-	vs, err := sfc.GetVenueStocks(os.Getenv("STOCKFIGHTER_VENUE"))
-	if err != nil || !vs.Ok {
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Connectivity broken", err)
-		}
-		fmt.Fprintln(os.Stderr, "Venue isn't up")
 	}
 	fmt.Fprintln(os.Stderr, "Available stocks")
 	for {
